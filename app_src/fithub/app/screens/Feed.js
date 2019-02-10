56f2cc4880ee90e7f22fd7122ee803e21193c0c9
asionnn <collin.li0499@gmail.com>
2019-02-10 17:58:13 -0500

    algorithm to log standard workout, added more screens

---
+++ app_src/fithub/app/screens/Feed.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    View,
+    StatusBar,
+    Platform
+} from 'react-native';
+
+export default class Feed extends React.Component {
+
+    render() {
+        return (
+            <Text>feed Screen</Text>
+        )
+    }
+}
