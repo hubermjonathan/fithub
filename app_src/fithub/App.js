d4271b1d6dcab4d6bd85382664bbde083a0fe47d
Jon Huber <hubermjonathan@gmail.com>
2019-02-09 20:58:26 -0500

    implemented blank calendar and day views

--- app_src/fithub/App.js
+++ app_src/fithub/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  StatusBar,
+  Platform
+} from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      workouts: {},
+    }
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>pen up App.js to start working on your app!</Text>
-      </View>
-    );
+    if(Platform.OS === 'ios') {
+      return (
+        <SafeAreaView style={styles.container}>
+          <StatusBar backgroundColor="white"/>
+          <Agenda
+            selected={'2019-02-09'}
+          />
+        </SafeAreaView>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar backgroundColor="white"/>
+          <Agenda
+            selected={'2019-02-09'}
+          />
+        </View>
+      );
+    }
   }
 }
 
@@ -15,7 +45,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
