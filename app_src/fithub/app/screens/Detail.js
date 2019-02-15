46df412de2dc2efd5209d31ba3658561748c8b35
Jon Huber <hubermjonathan@gmail.com>
2019-02-15 17:16:48 -0500

    beginning to work on detail UI

--- app_src/fithub/app/screens/Detail.js
+++ app_src/fithub/app/screens/Detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  View,
   SafeAreaView,
   Text,
   StyleSheet,
@@ -8,22 +9,31 @@ import {
 } from 'react-native';
 
 export default class Detail extends React.Component {
-    render() {
-      if(Platform.OS === 'ios') {
-        return (
-          <SafeAreaView style={styles.container}>
-            <Text>{this.props.navigation.getParam('name', 'workout-name')}</Text>
-            <Button onPress={() => this.props.navigation.goBack()} title="Go Back"/>
-          </SafeAreaView>
-        );
-      } else {
-        return (
-          <View style={styles.container}>
-            <Text>{this.props.navigation.getParam('name', 'workout-name')}</Text>
-          </View>
-        );
+  static navigationOptions = ({ navigation }) => {
+    console.log(navigation.getParam('name', 'Workout'));
+    return {
+      title: navigation.getParam('name', 'Workout'),
+      headerStyle: {
+        backgroundColor: "#eee",
+        color: "#000"
       }
+    };
+  };
+
+  render() {
+    if(Platform.OS === 'ios') {
+      return (
+        <SafeAreaView style={styles.container}>
+        </SafeAreaView>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>{this.props.navigation.getParam('name', 'workout-name')}</Text>
+        </View>
+      );
     }
+  }
 }
 
 const styles = StyleSheet.create({
