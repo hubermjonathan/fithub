2d8ae172d8bf7b981d9affa7aaf182aa5eca6724
Brian Long <brianlong912@gmail.com>
2019-02-15 16:13:24 -0500

    working on rendering from different class

--- app_src/fithub/app/Components/BottomBar.js
+++ app_src/fithub/app/Components/BottomBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Icon } from 'react-native-elements';
+import { Icon, View } from 'react-native-elements';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Profile from '../screens/Profile';
 
 class BottomBar extends React.Component{
+export default class BottomBar extends React.Component{
+  constructor(props){
+    super(props);
+  }
     render() {
         return (
             <View style={{
@@ -9,17 +16,63 @@ class BottomBar extends React.Component{
                 flexDirection: 'row',
                 justifyContent: 'flex-end',
                 alignItems: 'stretch',
+                justifyContent: 'space-around',
+                alignItems: 'flex-end',
             }}>
             <Icon
                 name="account"
+                name="book"
                 type="material-community"
                 size={20}
+                size={50}
+                // onPress={() => this.props.navigation.navigate('Details')}
             />
             <Icon
                 name="book"
+              name="plus"
+              type="entypo"
+              size={50}
+              //onPress={() => this.props.navigation.navigate('Details')}
+            />
+            <Icon
+              name="home"
+              type="material-community"
+              size={50}
+              //onPress={() => this.props.navigation.navigate('Details')}
+            />
+            <Icon
+              name="globe"
+              type="entypo"
+              size={50}
+              //onPress={() => this.props.navigation.navigate('Details')}
+            />
+            <Icon
+                name="account"
                 type="material-community"
+                size={50}
+                //onPress={() => this.props.navigation.navigate('Profile')}
             />
             </View>
         );
     }
-}
+}}
+
+module.export = BottomBar;
+
+/* const RootStack = createStackNavigator(
+    {
+      Home: App,
+      Details: Profile,
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
+
+  const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+} */
