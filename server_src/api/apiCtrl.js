0601488b684c97ecc60332e2a3d0ed79a5df9a27
Raymond Truong <raylintruong@gmail.com>
2019-02-19 22:18:34 -0500

    fixed header bug causing server to crash

--- server_src/api/apiCtrl.js
+++ server_src/api/apiCtrl.js
@@ -199,7 +199,6 @@ let logs = function logs(req, res) {
 
 //Return a users workouts
 let workouts = function workouts(req, res) {
-
   let db = connectToDb();
   db.once('open', () => {
     //query profile collection
@@ -207,12 +206,11 @@ let workouts = function workouts(req, res) {
     .findOne({uid : req.params.uid}, (err, workouts) => {
       if(err){
         res.status(500).send({message: "Error getting workouts"});
-      } else{
-        res.send(workouts);
+        return;
       }
     })
     .populate('workouts')
-    .exec((err, workouts) =>{
+    .exec((err, workouts) => {
       if(err){
         res.status(500).send({message: "Error getting workouts"});
       } else{
