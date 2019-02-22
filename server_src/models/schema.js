ce013131f83f33a472c1130644c0b6af5e846dc9
Andy Plank <aplank14@gmail.com>
2019-02-21 22:24:23 -0500

    Revert "changed schemas again for trevor"
    This reverts commit efe63acdc95bc0948e4e797abf7fd12eea79a2e4.

--- server_src/models/schema.js
+++ server_src/models/schema.js
@@ -16,57 +16,52 @@ let Profile = new Schema({
     pseudonym: { type: String, required: true },                        //Nickname
     email : { type: String, required: true },                           //User email
     avatar: { type: String, required: true },                           //User profile picture
+    workouts: [{ type: Schema.Types.ObjectId, ref: "Workout"}],         //Workout plans the user has submitted to the master list
+    exercises : { type: [{
+                            name: { type: String, required: true },
+                            description: { type: String, required: true },
+                            muscleGroups : { type: [String] , required: true},
+                        }], required : true },
+    logs : [{                                                           //Array of JSON objects which contain arrays of exercises
+                name: {type : String, required: true},
+                date : {type : String, required: true},
+                exercises : { type : [{           
+                    name: {type: String, required: true},
+                    reps: {type: [Number], required: true},
+                    weight: {type: [Number], required: true},                    
+                    isWarmup: {type: [Boolean], required: true} 
+                }], 
+                required: true, validate : err},
+           }],                           
     uid: { type: String, required: true },                              //Google unique user id
     token : { type: String, required: true },
-    workouts: [{ type: Schema.Types.ObjectId, ref: "WorkoutTemplate"}],         //Workout plans the user has submitted to the master list
-    exercises: [{ type: Schema.Types.ObjectId, ref: "ExerciseTemplate"}],         //Private exercises
-    logs: [{ type: Schema.Types.ObjectId, ref: "WorkoutData"}],         //Workout data
 });
 
-//Schema for creating a new exercise template (library)
-let ExerciseTemplate = new Schema({
-  name: {type: String, required: true},
-  muscle_group: {type: String, required: true},
-  equipment_type: {type: String, required: true},
-  sets: [{ type: Schema.Types.ObjectId, ref: "SetData", required: true}]
-});
-
-//Schema for creating a new workout template (library)
-let WorkoutTemplate = new Schema({
+//Schema for creating a new exercise
+let Exercise = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    exercises: [{ type: Schema.Types.ObjectId, ref: "ExerciseTemplate", required: true}]
+    muscleGroups : { type: [String] , required: true},
+});
+
+//Schema for creating a new workout
+let Workout = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    exercises: { type : [{           
+                    name: {type: String, required: true},
+                    reps: {type: [Number], required: true},
+                    isWarmup: {type: [Boolean], required: true} 
+                }], required: true},
     ownerUID : { type: String, required: true },
     likes : { type : Number }
-});
-
-let WorkoutData = new Schema({
-  name: {type: String, required: true},
-  date : {type : Date, required: true},
-  exercises: [{ type: Schema.Types.ObjectId, ref: "ExerciseData", required: true}]
-});
-
-let ExerciseData = new Schema({
-  name: {type: String, required: true},
-  muscle_group: {type: String, required: true},
-  equipment_type: {type: String, required: true},
-  sets: [{ type: Schema.Types.ObjectId, ref: "SetData", required: true}]
-});
-
-let SetData = new Schema({
-  weight: {type: Number, required: true},
-  reps: {type: Number, required: true},
-  isWarmup: {type: Boolean, required: true} 
 });
 
 //map the schemas to mongo collections
 let schemaCtrl = {
     Profile: mongoose.model("Profile", Profile),
-    ExerciseTemplate: mongoose.model("ExerciseTemplate", ExerciseTemplate),
-    WorkoutTemplate: mongoose.model("WorkoutTemplate", WorkoutTemplate),
-    WorkoutData: mongoose.model("WorkoutData", WorkoutData),
-    ExerciseData: mongoose.model("ExerciseData", ExerciseData),
-    SetData: mongoose.model("SetData", SetData),
+    Exercise: mongoose.model("Exercise", Exercise),
+    Workout: mongoose.model("Workout", Workout),
 }
 
 module.exports = schemaCtrl;
