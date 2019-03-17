fc3c9b49516c2c8390305e083d4f38e0656d3cca
Raymond Truong <raylintruong@gmail.com>
2019-03-17 00:52:25 -0400

    inverted edit api

--- server_src/api/apiCtrl.js
+++ server_src/api/apiCtrl.js
@@ -554,12 +554,12 @@ let editUsername = async function editUsername(req, res){
   if(!isValidated(req, res, user)){ console.log("Unauthorized request"); return; };
   let new_name = req.body.name;
   if(new_name.length > 32 || new_name.match(/^[a-z0-9]+$/i)){
+    return res.status(500).send({ "message": "Invalid name: name must be 32 alphanumeric characters or less"});
+  }
+  else{
     user.set('name', req.body.name);
     user.save().catch(err => {console.log(err);});
     return res.status(200).send({ "message": "Successfully changed name to " + new_name });
-  }
-  else{
-    return res.status(500).send({ "message": "Invalid name: name must be 32 alphanumeric characters or less"});
   }
 }
 
