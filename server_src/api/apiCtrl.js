8476276439316ebdb67875daa7ad4ad478ad5d48
Raymond Truong <raylintruong@gmail.com>
2019-04-17 20:13:46 -0400

    handled user not found in calorie and weight gets

--- server_src/api/apiCtrl.js
+++ server_src/api/apiCtrl.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const schemaCtrl = require('../models/schema');
-const url = "mongodb://admin:team5307@fithub-database-shard-00-00-3xylr.gcp.mongodb.net:27017,fithub-database-shard-00-01-3xylr.gcp.mongodb.net:27017,fithub-database-shard-00-02-3xylr.gcp.mongodb.net:27017/test?ssl=true&replicaSet=fithub-database-shard-0&authSource=admin&retryWrites=true";
+const url = "mongodb://admin:team5310@fithub-database-shard-00-00-3xylr.gcp.mongodb.net:27017,fithub-database-shard-00-01-3xylr.gcp.mongodb.net:27017,fithub-database-shard-00-02-3xylr.gcp.mongodb.net:27017/test?ssl=true&replicaSet=fithub-database-shard-0&authSource=admin&retryWrites=true";
 const passport = require('../config/passport');
 const moment = require('moment');
 moment().format();
@@ -1485,12 +1485,18 @@ let stats = function stats(req, res){
 let getCalories = async function getCalories(req,res){
   if(db.readyState==0){res.status(500).send({error: "Database connection is down."});return;}
   let user = await schemaCtrl.Profile.findById(req.params.id).catch(err => {console.log("invalid id");});
+  if(!user){
+    res.status(404).send({message: "user not found"});
+  }
   res.status(200).send(user.calories);
 }
 
 let getWeight = async function getWeight(req,res){
   if(db.readyState==0){res.status(500).send({error: "Database connection is down."});return;}
   let user = await schemaCtrl.Profile.findById(req.params.id).catch(err => {console.log("invalid id");});
+  if(!user){
+    res.status(404).send({message: "user not found"});
+  }
   res.status(200).send(user.weight);
 }
 
